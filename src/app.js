26739b02addc87375ad5f4023a96461df0585f50
cbhutia <cbhutia12@hotmail.com>
2023-11-20 19:53:03 +0100

    added in HTML with a button

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
-/* eslint-disable */
 window.onload = function() {
   //write your code here
-  console.log("Here are the different domains!");
+  document.querySelector("#generate").addEventListener("click", domainGenerator);
+  
+};
 
+
+function domainGenerator() {
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
   let extension = ['.com', '.net', '.us', '.io'];
 
+  let domains = []; 
+
   for (let p = 0; p < pronoun.length; ++p) {
     for (let a = 0; a < adj.length; ++a) {
       for (let n = 0; n < noun.length; ++n) {
         for (let e = 0; e < extension.length; ++e) {
-          console.log(pronoun[p] + adj[a] + noun[n] + extension[e]);
+          let domain = pronoun[p] + adj[a] + noun[n] + extension[e];
+          domains.push(domain);
+          console.log(domain);
         }
       }
     }
   }
+
+  document.querySelector("#domains").innerHTML = "<li>" + domains.join("</li><li>") + "</li>";
 };
